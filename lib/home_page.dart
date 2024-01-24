811166cf10f0e6c4c301e91725070ac844409171
thephotogenicbug <navinkumarachari@gmail.com>
2024-01-24 14:11:07 +0530

    Design Company Chips

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  final List<String> filters = const ['All', 'Adidas', 'Nike', 'Bata'];
+
   @override
   Widget build(BuildContext context) {
     const border = OutlineInputBorder(
@@ -14,11 +16,11 @@ class HomePage extends StatelessWidget {
       ),
     );
 
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
         child: Column(
           children: [
-            Row(
+            const Row(
               children: [
                 Padding(
                   padding: EdgeInsets.all(20.0),
@@ -38,6 +40,32 @@ class HomePage extends StatelessWidget {
                 ),
               ],
             ),
+            SizedBox(
+              height: 120,
+              child: ListView.builder(
+                itemCount: filters.length,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  final filter = filters[index];
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Chip(
+                      backgroundColor: const Color.fromRGBO(245, 247, 249, 1),
+                      side: const BorderSide(
+                        color: Color.fromRGBO(245, 247, 249, 1),
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 15),
+                      label: Text(filter),
+                      labelStyle: const TextStyle(fontSize: 16),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            )
           ],
         ),
       ),
