6896bff4de6d7f94b80963b6e5334bd109828de2
thephotogenicbug <navinkumarachari@gmail.com>
2024-01-25 11:55:45 +0530

    Display List of Products in ListView.builder

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/global_variables.dart';
+import 'package:shop_app/product_card.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -87,7 +89,22 @@ class _HomePageState extends State<HomePage> {
                 },
               ),
             ),
-            
+            Expanded(
+              child: ListView.builder(
+                itemCount: products.length,
+                itemBuilder: (context, index) {
+                  final product = products[index];
+                  return ProductCard(
+                    title: product['title'] as String,
+                    price: product['price'] as double,
+                    image: product['imageUrl'] as String,
+                    backgroundColor: index.isEven
+                        ? const Color.fromRGBO(216, 240, 253, 1)
+                        : const Color.fromRGBO(245, 247, 249, 1),
+                  );
+                },
+              ),
+            )
           ],
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,12 @@ class MyApp extends StatelessWidget {
           hintStyle: TextStyle(fontWeight: FontWeight.bold),
           prefixIconColor: Color.fromRGBO(119, 119, 119, 1),
         ),
+        textTheme: const TextTheme(
+          titleMedium: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+          bodySmall: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)
+        
+        ),
+        
       ),
       home: const HomePage(),
     );
