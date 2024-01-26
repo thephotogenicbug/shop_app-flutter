193abc758f441bea452fe29b5b6903433af9f774
thephotogenicbug <navinkumarachari@gmail.com>
2024-01-26 11:53:48 +0530

    Design CartPage

--- lib/cart_page.dart
+++ lib/cart_page.dart
@@ -1,13 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/global_variables.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Cart Page'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Cart"),
+      ),
+      body: ListView.builder(
+        itemCount: cart.length,
+        itemBuilder: (context, index) {
+          final cartItem = cart[index];
+
+          return ListTile(
+            leading: CircleAvatar(
+              backgroundImage: AssetImage(cartItem['imageUrl'] as String),
+              radius: 30,
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.delete,
+                color: Colors.red,
+              ),
+            ),
+            title: Text(
+              cartItem['title'].toString(),
+              style: Theme.of(context).textTheme.bodySmall,
+            ),
+            subtitle: Text('Size: ${cartItem['size']}'),
+          );
+        },
       ),
     );
   }
